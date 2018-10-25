5ea16adf224acffefb66abc80fd37b05e159e451
Tori Jones <Tori@Trystian-Amedee-Joness-MacBook-Pro.local>
2018-10-25 18:57:26 -0500

    done

---
+++ 9_3.js
@@ -0,0 +1,20 @@
+var evens = [2, 4, 6, 8];
+var odds = [1, 3, 5, 7];
+var primes = [2,3,5,7,11];
+
+var allnumbers = [...primes , ...evens , ...odds];
+    console.log("Array of all numbers" + allnumbers);
+    console.log("Array of Primes" + primes);
+
+var firstName = "James";
+var lastName = "Bond";
+Var country = "England";
+var city = "New York";
+var twitter = "@jbond";
+    console.log(firstName, lastName, country, city, twiiter);
+
+var player1 = paul;
+var player2 = andy;
+var player3 = darrell;
+var player4 = jim;
+    console.log(player1, player2, player3, player4);
